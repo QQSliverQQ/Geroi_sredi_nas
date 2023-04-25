0c03e10f557e8a720d81a76e85d17c787269414c
QQSliverQQ <112465930+QQSliverQQ@users.noreply.github.com>
2023-04-26 01:06:07 +0500

    Add files via upload

---
+++ js/hero_scroll_toggle.js
@@ -0,0 +1,24 @@
+var shadow = "box-shadow: 0 0 10px 5px gray;";
+//Создаю переменные основных блоков с войнами
+var hero_lipatov = document.getElementById('hero_lipatov');
+var hero_demishev = document.getElementById('hero_demishev');
+var hero_zazdravnih = document.getElementById('hero_zazdravnih');
+var hero_malgin = document.getElementById('hero_malgin');
+var hero_plasynov = document.getElementById('hero_plasynov');
+//Массив картинок с героями
+var hero_scroll_img = document.getElementsByClassName('hero_scroll_img');
+//Массив с войнами
+var all_hero = [hero_lipatov, hero_demishev, hero_zazdravnih, hero_malgin, hero_plasynov];
+
+var lipatov = false;
+var demishev = false;
+var zazdravnih = false;
+var malgin = false;
+var plasynov = false;
+
+//Обрабатываю событие клика по картинкам с войнами
+function hero_scroll_img_click(e){
+  switch (e.id) {
+    
+  }
+}
